206468cbf084a99b603f21fade6473ae9937e87a
artur <user@instant-contiki.(none)>
2015-05-10 09:36:25 +0300

    Fixed sensor.c code errors

--- IASB/sensors/sensors.c
+++ IASB/sensors/sensors.c
@@ -13,13 +13,7 @@
 
 /* === MACROS / DEFINES ==================================================== */
 
-/** macro for printf */
-#define PRINT(...)    printf(__VA_ARGS__)
-
 /* === GLOBALS ============================================================= */
-
-/** Setup a File stream with putchar() and getchar() functionality over USB */
-//FILE usb_stream = FDEV_SETUP_STREAM(usb_putc_std, usb_getc_std, _FDEV_SETUP_RW);
 
 /* temperature sensor value */
 static temperature_t temp;
@@ -35,36 +29,6 @@ static acceleration_t accel;
 /* === IMPLEMENTATION ====================================================== */
 
 /**
- * @brief Initialize the hardware
- *
- * Initializes:
- *    - IO
- *    - USB
- *    - TWI interface
- *    - sensors
- *
- */
-static void app_init(void)
-{
-   /* initialize LEDs and stuff */
-   io_init();
-
-   /* initialize USB interface */
-//   usb_init();
-
-   /* redirect standard input/output to USB stream */
-//   stdin = stdout = &usb_stream;
-
-   /* initialize TWI interface and connected sensors */
-//   TWI_MasterInit();
-//
-//   BMA150_Init();
-//   ISL29020_Init();
-//   TMP102_Init();
-}
-
-
-/**
  * @brief Helper function for console printout
  *
  * Prints the measured sensor parameters values.
@@ -73,25 +37,25 @@ static void app_init(void)
 static void print_results(void)
 {
    /* print current temperature */
-   printf("Temperature: %c%d.%02d C\n", (temp->sign ? '-' : '+'),
-										temp->integralDigit,
-										temp->fractionalDigit);
+   printf("Temperature: %c%d.%02d C\n", (temp.sign ? '-' : '+'),
+										temp.integralDigit,
+										temp.fractionalDigit);
 
    /* print current luminosity sensor value */
-   printf("Luminosity: %d Lux\n", *lumi);
+   printf("Luminosity: %d Lux\n", lumi);
 
    /* print current acceleration values */
-   printf("Acceleration: x: %c%d.%02d g\n", accel->acc_x_sign ? '-' : '+',
-										accel->acc_x_integral,
-										accel->acc_x_fractional);
+   printf("Acceleration: x: %c%d.%02d g\n", accel.acc_x_sign ? '-' : '+',
+										accel.acc_x_integral,
+										accel.acc_x_fractional);
 
-   printf("              y: %c%d.%02d g\n", accel->acc_y_sign ? '-' : '+',
-										accel->acc_y_integral,
-										accel->acc_y_fractional);
+   printf("              y: %c%d.%02d g\n", accel.acc_y_sign ? '-' : '+',
+										accel.acc_y_integral,
+										accel.acc_y_fractional);
 
-   printf("              z: %c%d.%02d g\n", accel->acc_z_sign ? '-' : '+',
-										accel->acc_z_integral,
-										accel->acc_z_fractional);
+   printf("              z: %c%d.%02d g\n", accel.acc_z_sign ? '-' : '+',
+										accel.acc_z_integral,
+										accel.acc_z_fractional);
 }
 
 
@@ -132,8 +96,8 @@ PROCESS_THREAD(sensors_process, ev, data)
 
   PROCESS_BEGIN();
 
-  /* initialize hardware and all sensors */
-  app_init();
+  /* initialize LEDs and stuff */
+  io_init();
 
   /* loop infinite */
   while(1)
